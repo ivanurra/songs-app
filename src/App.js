0d5f879cd58a84606a178e57431367b412c60668
Ivan Urra <ivan.urra.wd@gmail.com>
2021-08-22 12:45:56 +0200

    start proj

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Hi friends!</h2>
     </div>
   );
 }
 
 export default App;
+
+
+{/* <a href={`https://${info.strFacebook}`} target="_blank" rel="noopener noreferrer">
+    <i className="fab fa-facebook"></i>
+</a>
+<a href={`https://${info.strTwitter}`} target="_blank" rel="noopener noreferrer">
+    <i className="fab fa-twitter"></i>
+</a>
+<a href={`${info.strLastFMChart}`} target="_blank" rel="noopener noreferrer">
+    <i className="fab fa-lastfm"></i>
+</a> */}
